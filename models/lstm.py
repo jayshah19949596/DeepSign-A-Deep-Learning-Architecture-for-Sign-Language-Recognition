99fa186894c090e1abeee5f2d116762612f0a1bd
Jai Shah <jai.s@somaiya.edu>
2018-03-27 03:00:25 -0500

    lstm training and testing successful

--- models/lstm.py
+++ models/lstm.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 
 
-class Bi_LSTM(object):
+class RecurrentNetwork(object):
 
-    def __init__(self, lstm_size, batch_len, output_nodes, keep_prob, learning_rate):
+    def __init__(self, lstm_size, batch_len, output_nodes, learning_rate):
         self.inputs_ = tf.placeholder(tf.float32, shape=[batch_len, None, 512], name='lstm_inputs')
         self.targets_ = tf.placeholder(tf.int32, [batch_len], name='lstm_targets')
         self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
@@ -13,15 +13,11 @@ class Bi_LSTM(object):
         self.batch_len = batch_len
         self.nodes = []
         self.loss = None
-        self.output_fw = None
-        self.output_bw = None
         self.optimizer = None
         self.y_one_hot = None
         self.predictions = None
-        self.final_state_fw = None
-        self.final_state_bw = None
-        self.initial_state_fw = None
-        self.initial_state_bw = None
+        self.final_state = None
+        self.initial_state = None
 
     def process_node_names(self):
         print("===================================")
@@ -33,26 +29,16 @@ class Bi_LSTM(object):
 
     def build_model(self):
 
-        self.initial_state_fw, cell_fw = self.lstm_layers(self.batch_len, self.keep_prob,
-                                                          self.lstm_size, number_of_layers=2)
+        self.initial_state, cell = self.lstm_layers(self.batch_len, self.keep_prob,  self.lstm_size, number_of_layers=2)
 
-        self.initial_state_bw, cell_bw = self.lstm_layers(self.batch_len, self.keep_prob,
-                                                          self.lstm_size, number_of_layers=2)
+        outputs, state = tf.nn.dynamic_rnn(cell, self.inputs_, initial_state=self.initial_state)
+        self.final_state = state
 
-        outputs, states = tf.nn.bidirectional_dynamic_rnn(cell_fw=cell_fw,
-                                                          cell_bw=cell_bw,
-                                                          inputs=self.inputs_,
-                                                          initial_state_fw=self.initial_state_fw,
-                                                          initial_state_bw=self.initial_state_bw)
+        self.predictions = tf.contrib.layers.fully_connected(outputs[:, -1], self.output_nodes, activation_fn=tf.sigmoid)
 
-        self.output_fw, self.output_bw = outputs
-        self.final_state_fw, self.final_state_bw = states
-
-        outputs = tf.concat(outputs, 2)
-        self.predictions = tf.contrib.layers.fully_connected(outputs[:, -1], self.output_nodes,
-                                                             activation_fn=tf.sigmoid)
         self.build_cost(self.predictions)
         self.build_optimizer()
+        self.process_node_names()
 
     def build_cost(self, predictions):
         self.y_one_hot = tf.one_hot(self.targets_, self.output_nodes)
@@ -68,7 +54,6 @@ class Bi_LSTM(object):
 
         def lstm_cell():
             lstm_nodes = tf.contrib.rnn.BasicLSTMCell(lstm_size)
-
             drop = tf.contrib.rnn.DropoutWrapper(lstm_nodes, output_keep_prob=keep_prob)
             return drop
 
@@ -80,6 +65,5 @@ class Bi_LSTM(object):
 
 
 if __name__ == '__main__':
-  rnn = Bi_LSTM(lstm_size=128, batch_len=1, output_nodes=14, keep_prob=0.85, learning_rate=0.001)
+  rnn = RecurrentNetwork(lstm_size=128, batch_len=3, output_nodes=99, learning_rate=0.001)
   rnn.build_model()
-
